2972cccdd0d485f96ad2e41c185d25fdab9b5028
Menezes999 <cristianomenzs@gmail.com>
2024-12-14 18:02:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("StormBots está pronta para impressionar!");
-});
+const timerElement = document.getElementById("timer");
+
+function startCountdown(durationInMinutes) {
+    let timer = durationInMinutes * 60;
+    const interval = setInterval(() => {
+        const minutes = Math.floor(timer / 60);
+        const seconds = timer % 60;
+
+        timerElement.textContent = `${minutes}m ${seconds}s`;
+
+        if (--timer < 0) {
+            clearInterval(interval);
+            timerElement.textContent = "Oferta Expirada!";
+        }
+    }, 1000);
+}
+
+startCountdown(60); // Timer de 1 hora
