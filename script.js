87cff79c64a70c598fc899a22728ba81e0fc9924
Menezes999 <cristianomenzs@gmail.com>
2024-12-16 04:25:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const sideDrawer = document.querySelector('.side-drawer');
-    const closeBtn = document.querySelector('.close-btn');
-
-    // Abrir a gaveta lateral
-    menuToggle.addEventListener('click', function() {
-        sideDrawer.classList.add('open');
-    });
-
-    // Fechar a gaveta lateral
-    closeBtn.addEventListener('click', function() {
-        sideDrawer.classList.remove('open');
-    });
-});
+function toggleDrawer() {
+    const sideDrawer = document.getElementById('sideDrawer');
+    sideDrawer.classList.toggle('open'); // Adiciona ou remove a classe 'open'
+}
