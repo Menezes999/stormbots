e2a7e30efb69b89877919f010725aa8f3815cb31
Menezes999 <cristianomenzs@gmail.com>
2024-12-16 04:42:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
+// Função para abrir e fechar a gaveta lateral
 function toggleDrawer() {
     const sideDrawer = document.getElementById('sideDrawer');
-    sideDrawer.classList.toggle('open'); // Adiciona ou remove a classe 'open'
+    sideDrawer.classList.toggle('open'); // Adiciona ou remove a classe 'open' que controla o estado da gaveta
 }
