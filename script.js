eeed3d35159bed7a62d304bab678131f6d3ddfb2
Menezes999 <cristianomenzs@gmail.com>
2024-12-16 07:06:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,3 @@ function toggleDrawer() {
     const drawer = document.getElementById('side-drawer');
     drawer.classList.toggle('open');
 }
-
-document.addEventListener("contextmenu", function(e) {
-    e.preventDefault(); // Desabilita o menu de contexto
-});
